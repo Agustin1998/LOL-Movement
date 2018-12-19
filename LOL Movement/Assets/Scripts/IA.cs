2d922f3e1b7f9fddf64753bf0517f1f8c51a1815
RICHARD-PC\RICHARD <ta3agustinruizrojo@gmail.com>
2018-12-19 22:46:08 +0100

    001

---
+++ LOL Movement/Assets/Scripts/IA.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour
+{
+
+    public enum States { Idle, Walk};
+    public States miestado = States.Idle;
+    public GameObject Player;
+    
+    public Transform target;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (miestado)
+        {
+            case States.Idle:
+                Idle();
+                break;
+            case States.Walk:
+                Walk();
+                break;
+            default:
+                print("Incorrect State");
+                break;
+        }
+    }
+
+    void Idle()
+
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) > 1 && Vector3.Distance(transform.position, Player.transform.position) < 8)
+        {
+            miestado = States.Walk;
+        }
+
+    }
+
+
+    void Walk()
+    {     
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > 8)
+        {
+            miestado = States.Idle;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+
+
+    }
+        
+
+}
---
+++ LOL Movement/Assets/Scripts/LOLMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LOLMovement : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 10;
+    
+    
+
+    // Use this for initialization
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(hit.distance);
+            transform.position = new Vector3(hit.point.x, 0.6f, hit.point.z);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position, speed * Time.deltaTime);
+    }
+}
